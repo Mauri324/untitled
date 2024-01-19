9f24f21db82cf4efa5ed0a0219a44baf36a123be
Mauri324 <156316981+Mauri324@users.noreply.github.com>
2024-01-19 13:11:53 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,72 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . b b . . . 
+        . . b 5 5 b . . 
+        . . b 4 4 b . . 
+        . . b 5 5 b . . 
+        . . b 5 5 b . . 
+        . . c 5 5 c . . 
+        . . c 4 4 c . . 
+        . . . f f . . . 
+        `, mySprite, 0, -50)
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(mySprite2)
+    sprites.destroy(mySprite2, effects.fire, 500)
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    sprites.destroy(otherSprite, effects.fire, 500)
+    scene.cameraShake(4, 500)
+})
+let mySprite2: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+game.splash("Star Treck", "Pulsa a per comenzar i disparar")
+effects.confetti.startScreenEffect()
+mySprite = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 2 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . e 2 . . . . . . . 
+    . . . . . . e e 4 e . . . . . . 
+    . . . . . . e 2 4 e . . . . . . 
+    . . . . . c c c e e e . . . . . 
+    . . . . e e 2 2 2 4 e e . . . . 
+    . . c f f f c c e e f f e e . . 
+    . c c c c e e 2 2 2 2 4 2 e e . 
+    c c c c c c e e 2 2 2 4 2 2 e e 
+    c c c c c c e e 2 2 2 2 4 2 e e 
+    `, SpriteKind.Player)
+mySprite.setPosition(77, 51)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setStayInScreen(true)
+info.setLife(3)
+game.onUpdateInterval(1000, function () {
+    mySprite2 = sprites.createProjectileFromSide(img`
+        . . . . . . . c c c a c . . . . 
+        . . c c b b b a c a a a c . . . 
+        . c c a b a c b a a a b c c . . 
+        . c a b c f f f b a b b b a . . 
+        . c a c f f f 8 a b b b b b a . 
+        . c a 8 f f 8 c a b b b b b a . 
+        c c c a c c c c a b c f a b c c 
+        c c a a a c c c a c f f c b b a 
+        c c a b 6 a c c a f f c c b b a 
+        c a b c 8 6 c c a a a b b c b c 
+        c a c f f a c c a f a c c c b . 
+        c a 8 f c c b a f f c b c c c . 
+        . c b c c c c b f c a b b a c . 
+        . . a b b b b b b b b b b b c . 
+        . . . c c c c b b b b b c c . . 
+        . . . . . . . . c b b c . . . . 
+        `, 0, 50)
+    mySprite2.x += randint(0, scene.screenWidth())
+    mySprite2.setKind(SpriteKind.Enemy)
+})
